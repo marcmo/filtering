9e8983056484130476e32a9155ce422e1960c33b
oliver <oliver.mueller@gmail.com>
2010-05-07 06:17:42 +0200

    removed unnecessary files

--- lazyfileio.hs
+++
@@ -1,9 +0,0 @@
-import Data.Char(toUpper)
-
-main = do 
-       inpStr <- readFile "input.txt"
-       let xs = lines inpStr
-       writeFile "output.txt" (processData $ head $ xs)
-
-processData :: String -> String
-processData = map toUpper
